9159893a29c2d8e11f94965fcd761a5fb9d29ee1
jeevaphysicist <jeevaphysicist@gmail.com>
2024-05-03 08:06:38 +0530

    first commit

---
+++ components/HomeComponents/Herosection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <div className='text-[50px] font-bold flex items-center justify-center h-[100vh] '>Herosection</div>
+  )
+}
+
+export default Herosection
